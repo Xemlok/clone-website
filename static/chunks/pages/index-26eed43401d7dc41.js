f1cc56a2ceb2505b9fa35c3f0b208d111cb515da
Xemlok <svechammd@gmail.com>
2025-11-12 22:09:49 +0100

    Add files via upload

---
+++ static/chunks/pages/index-26eed43401d7dc41.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{9404:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(1877)}])},1877:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return e}});var t=_(5210),e=!0;u.default=t.default}},function(n){n.O(0,[764,838,996,210,774,888,179],function(){return n(n.s=9404)}),_N_E=n.O()}]);
